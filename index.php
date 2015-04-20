e7aef0bd9f859df3392365b15c13d5a9b567feca
lordcuetox <delcueto@gmail.com>
2015-04-19 20:38:55 -0500

    se trabaja la pagina principal

--- index.php
+++ index.php
@@ -4,14 +4,45 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name=”description” content="Página oficial de Masoneria Sin Fronteras">
+        <meta name=”keywords” content="Masoneria, MSF, Masones">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MSF| Masoneria Sin Fronteras</title>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <header>
+            <figure id="icono"><img src="img/encabezado/msficono.jpg"></figure>  
+            <figure id="twitter"><img src="img/encabezado/twitter.png"></figure> 
+            <figure id="facebook"><img src="img/encabezado/facebook.png"></figure>
+            <figure id="blog"><img src="img/encabezado/msficono.jpg"></figure> 
+            <figure id="contactanos"><img src="img/encabezado/msficono.jpg"></figure> 
+        </header>
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Quiénes Somos</li>
+                <li>Directorio</li>
+                <li>Clasificados</li>
+                <li>Biblioteca</li>
+            </ul> 
+        </nav>
+        <section id="eventos">
+            
+        </section>
+        <section id="contenedor">
+            <article id="noticicias"></article>
+            <article id="directorio"></article>
+             <article id="clasificados"></article>
+              <article id="biblioteca"></article>
+        </section>
+         <aside id="clasiPrincipal"></aside>
+         <footer></footer>
     </body>
 </html>
