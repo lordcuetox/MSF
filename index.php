213849b903f4c1d302f02e5ddfd38dfdbe4b2bc6
lordcuetox <delcueto@gmail.com>
2015-04-24 17:05:33 -0500

    se agrega el bootstrap

--- index.php
+++ index.php
@@ -12,17 +12,23 @@ and open the template in the editor.
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MSF| Masoneria Sin Fronteras</title>
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="lib/bootstrap-3.3.4-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
+        <script src="lib/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+        <script src="js/JQuery/jquery-1.11.2.min.js"></script>
+            <!-- librerías opcionales que activan el soporte de HTML5 para IE8 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     </head>
     <body>
         <header>
-            <figure id="icono"><img src="img/encabezado/msficono.jpg"></figure>  
-            <figure id="twitter"><img src="img/encabezado/twitter.png"></figure> 
-            <figure id="facebook"><img src="img/encabezado/facebook.png"></figure>
-            <figure id="blog"><img src="img/encabezado/msficono.jpg"></figure> 
-            <figure id="contactanos"><img src="img/encabezado/msficono.jpg"></figure> 
+            <figure id="icono"><img src="img/encabezado/msficono.jpg" class="img-responsive"></figure>  
+            <figure id="twitter"><img src="img/encabezado/twitter.png" class="img-responsive"></figure> 
+            <figure id="facebook"><img src="img/encabezado/facebook.png" class="img-responsive"></figure>
+            <figure id="blog"><img src="img/encabezado/msficono.jpg" class="img-responsive"></figure> 
+            <figure id="contactanos"><img src="img/encabezado/msficono.jpg" class="img-responsive"></figure> 
         </header>
         <nav>
             <ul>
