f1587d4588ad9b60ca9a8be90bc153fbc52d0878
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-09 14:14:56 -0500

    Modificaciones para que se use includeHeader e includeFooter.

--- php/ideario.php
+++ php/ideario.php
@@ -1,4 +1,3 @@
-
 <html lang="es">
     <head>
         <meta charset="UTF-8">
@@ -12,7 +11,6 @@
         <link rel="stylesheet" href="../lib/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
         <script src="../js/jQuery/jquery-1.11.2.min.js"></script>
         <script src="../lib/bootstrap-3.3.4-dist/js/bootstrap.min.js" ></script>
-
         <!-- librerías opcionales que activan el soporte de HTML5 para IE8 -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -20,56 +18,8 @@
         <![endif]-->
     </head>
     <body>
-
-        <div class="container">   
-            <div class="row">
-                <div class="col-md-12 text-right" id="iconos_redes_sociales">
-                    <img src="../img/encabezado/msficono.jpg" alt="Logo MSF " class="img-responsive" id="logo"/>
-                    <a href="https://www.facebook.com/masoneria.sinfronteraas" target="_blank"><img src="../img/encabezado/Facebook-icon.png" class="img-responsive " alt="Facebook"/></a>
-                    <a href="https://twitter.com/masinfronteras" target="_blank"><img src="../img/encabezado/Twitter-icon.png" class="img-responsive" alt="Twitter"/></a>
-                    <a href="http://masoneriasinfronteras.blogspot.mx" target="_blank"><img src="../img/encabezado/Blogger-icon.png" class="img-responsive" alt="Blogspot"/></a>
-                </div>
-            </div>
-
-
-
-
-            <nav class="navbar navbar-inverse" >
-                <div class="container-fluid" id="barra_nav">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>   
-                            <span class="icon-bar"></span> 
-                        </button>
-                        <a class="navbar-brand" href="../index.php" id="fondo_barra" >MSF</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav">
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Quiénes Somos <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                <li><a href="ideario.php">Ideario</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Directorio</a></li>
-                            <li><a href="#">Servicios Profesionales</a></li>
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Biblioteca <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Libros</a></li>
-                                    <li><a href="#">Hemeroteca</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Inicia Sesión</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <div class="container"> 
+            <?php include 'includeHeader.php'; ?>
             <div><img class="img-responsive" id="foto_ideario"  src="../img/quienes_somos/ideario.JPG"></img></div>
             <div id="ideario">
                 <ol>
@@ -77,8 +27,6 @@
                     <li><p class="texto_ideario">Como primer paso para la creación de M:. S:. F:. RECHAZAMOS de manera absoluta y determinante el término IRREGULARIDAD dentro de la masonería, porque va en contra de la IGUALDAD que tanto pregona esta Augusta Institución. Por tal motivo, todos los que militen dentro de  M:. S:. F:. son iguales, sin diferenciar géneros, ritos, logias, religiones, razas o idiomas.</p></li>
                 </ol>
             </div>
-            <footer>
-                <p class="text-muted text-center">Copyright <?php echo date("Y"); ?>| Masoneria Sin Fronteras| Powered By WEBXICO & Cuetox</p>
-            </footer>
+            <?php include 'includeFooter.php'; ?>
         </div>
 </html>
