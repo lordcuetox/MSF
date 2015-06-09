3d4080f0a659d2072ce0786fd9ed313c5c80403c
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-09 14:15:12 -0500

    Modificaciones para que se use includeHeader e includeFooter.

--- index.php
+++ index.php
@@ -25,58 +25,8 @@ and open the template in the editor.
         <![endif]-->
     </head>
     <body>
-
-        <div class="container">   
-            <div class="row">
-                <div class="col-md-12 text-right" id="iconos_redes_sociales">
-                    <img src="img/encabezado/msficono.jpg" alt="Logo MSF " class="img-responsive" id="logo"/>
-                    <a href="https://www.facebook.com/masoneria.sinfronteraas" target="_blank"><img src="img/encabezado/Facebook-icon.png" class="img-responsive " alt="Facebook"/></a>
-                    <a href="https://twitter.com/masinfronteras" target="_blank"><img src="img/encabezado/Twitter-icon.png" class="img-responsive" alt="Twitter"/></a>
-                    <a href="http://masoneriasinfronteras.blogspot.mx" target="_blank"><img src="img/encabezado/Blogger-icon.png" class="img-responsive" alt="Blogspot"/></a>
-                </div>
-            </div>
-
-
-
-
-            <nav class="navbar navbar-inverse" >
-                <div class="container-fluid" id="barra_nav">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>   
-                            <span class="icon-bar"></span> 
-                        </button>
-                        <a class="navbar-brand" href="index.php" id="fondo_barra" >MSF</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav">
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Quiénes Somos <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="php/ideario.php">Ideario</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Directorio</a></li>
-                            <li><a href="#">Servicios Profesionales</a></li>
-                            <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Biblioteca <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Libros</a></li>
-                                    <li><a href="#">Hemeroteca</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrate</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Inicia Sesión</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
-
+        <div class="container"> 
+            <?php include './php/includeHeader.php'; ?>
             <div class="row">
                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
@@ -172,7 +122,6 @@ and open the template in the editor.
                     </div>
 
                 </div>
-
                 <div class="col-lg-4" id="anuncios">
                     <div class="row">
                         <p>Profesionales a tu servicio</p>
@@ -207,15 +156,9 @@ and open the template in the editor.
                             <img src="img/clasificados/3.jpg" class="img-responsive"/>
                         </div> 
                     </div>
-
-
-
-
                 </div>
             </div>
-            <footer>
-                <p class="text-muted text-center">Copyright <?php echo date("Y"); ?>| Masoneria Sin Fronteras| Powered By WEBXICO & Cuetox</p>
-            </footer>
+            <?php include './php/includeFooter.php'; ?>
         </div>
     </body>
 </html>
