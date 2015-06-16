055a3402ad40966c0112570fa571a2c62250b056
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-16 12:56:17 -0500

    Modifiaciones para que use un objeto de la clase Evento.

--- php/eventos_id.php
+++ php/eventos_id.php
@@ -1,113 +1,113 @@
 <?php
 require_once '../clases/UtilDB.php';
+require_once '../clases/Evento.php';
+$evento = "";
 $sql = "";
-$rst = NULL;
 $msg = "";
 
 if (isset($_GET['id'])) {
-    $sql = "SELECT * FROM eventos WHERE cve_evento = " . $_GET['id'];
-    $rst = UtilDB::ejecutaConsulta($sql);
+    $evento = new Evento((int) $_GET['id']);
 } else {
+    $evento = new Evento();
     $msg = "Lo sentimos, su busqueda no tiene resultados";
 }
 
-if ($rst->rowCount() > 0) {
-    foreach ($rst as $row) {
-        ?>
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title"><?php echo($row['nombre']); ?></h4>
-        </div>
-        <div class="modal-body">
-            <div class="te">
-                <div id="carousel-eventos-msf" class="carousel slide" data-ride="carousel">
+if ($evento->getCveEvento() > 0) {
+    ?>
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title"><?php echo($evento->getNombre()); ?></h4>
+    </div>
+    <div class="modal-body">
+        <div class="te">
+            <div id="carousel-eventos-msf" class="carousel slide" data-ride="carousel">
 
 
-                    <?php
-                    if ($row['foto_principal'] != "" && $row['foto1'] != "" && $row['foto2'] != "" && $row['foto3'] != "" && $row['foto4'] != "") {
-                        ?>
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="2"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="3"></li>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="<?php echo($row['foto1']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto2']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto3']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 3">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto4']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 4">
-                            </div>
-                        </div>    
-                        <?php
-                    } elseif ($row['foto_principal'] != "" && $row['foto1'] != "" && $row['foto2'] != "" && $row['foto3'] != "") {
-                        ?>
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="2"></li>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="<?php echo($row['foto1']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto2']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto3']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 3">
-                            </div>
-                        </div>    
-                        <?php
-                    } elseif ($row['foto_principal'] != "" && $row['foto1'] != "" && $row['foto2'] != "") {
-                        ?>
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="<?php echo($row['foto1']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                            <div class="item">
-                                <img src="<?php echo($row['foto2']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 2">
-                            </div>
-                        </div>
-                        <?php
-                    } elseif ($row['foto_principal'] != "" && $row['foto1'] != "") {
-                        ?>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="<?php echo($row['foto1']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 1">
-                            </div>
-                        </div>
-                        <?php
-                    } else {
-                        ?>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <img src="<?php echo($row['foto_principal']); ?>" alt="<?php echo($row['nombre']); ?> - Imagen 1">
-                            </div>
-                        </div>
-                        <?php
-                    }
+                <?php
+                if ($evento->getFotoPrincipal() != "" && $evento->getFoto1() != "" && $evento->getFoto2() != "" && $evento->getFoto3() != "" && $evento->getFoto4() != "") {
                     ?>
-                    <!-- SI AL MENOS TIENE 2 IMAGENES QUE MUESTRE LOS CONTROLES DE NAVEGACION ATRAS Y ADELANTE -->
-                    <?php if($row['foto1'] != "" && $row['foto2'] != ""){?>
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="2"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="3"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="<?php echo($evento->getFoto1()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto2()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto3()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 3">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto4()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 4">
+                        </div>
+                    </div>    
+                    <?php
+                } elseif ($evento->getFotoPrincipal() != "" && $evento->getFoto1() != "" && $evento->getFoto2() != "" && $evento->getFoto3() != "") {
+                    ?>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="<?php echo($evento->getFoto1()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto2()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto3()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 3">
+                        </div>
+                    </div>    
+                    <?php
+                } elseif ($evento->getFotoPrincipal() != "" && $evento->getFoto1() != "" && $evento->getFoto2() != "") {
+                    ?>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-eventos-msf" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-eventos-msf" data-slide-to="1"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="<?php echo($evento->getFoto1()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                        <div class="item">
+                            <img src="<?php echo($evento->getFoto2()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 2">
+                        </div>
+                    </div>
+                    <?php
+                } elseif ($evento->getFotoPrincipal() != "" && $evento->getFoto1() != "") {
+                    ?>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="<?php echo($evento->getFoto1()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 1">
+                        </div>
+                    </div>
+                    <?php
+                } else {
+                    ?>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="<?php echo($evento->getFotoPrincipal()); ?>" alt="<?php echo($evento->getNombre()); ?> - Imagen 1">
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+                <!-- SI AL MENOS TIENE 2 IMAGENES QUE MUESTRE LOS CONTROLES DE NAVEGACION ATRAS Y ADELANTE -->
+                <?php if ($evento->getFoto1() != "" && $evento->getFoto2() != "") { ?>
                     <!-- Controls -->
                     <a class="left carousel-control" href="#carousel-eventos-msf" role="button" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -117,18 +117,17 @@ if ($rst->rowCount() > 0) {
                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                     </a>
-                    <?php }?>
-                </div>
-                <br/>
-                <p class="negritas">Descripción:</p>
-                <p><?php echo($row['descripcion']); ?></p>
+                <?php } ?>
             </div>
+            <br/>
+            <p class="negritas">Descripción:</p>
+            <p><?php echo($evento->getDescripcion()); ?></p>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-        </div>
-        <?php
-    }
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+    </div>
+    <?php
 } else {
     ?>    
     <div class="modal-header">
@@ -143,5 +142,4 @@ if ($rst->rowCount() > 0) {
     </div>
     <?php
 }
-$rst->closeCursor();
 ?>
