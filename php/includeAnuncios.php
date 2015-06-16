402739a9c38909b5af9608c9625718c915dec4d3
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-16 10:38:18 -0500

    Versión inicial.

---
+++ php/includeAnuncios.php
@@ -0,0 +1,36 @@
+<?php $path = "/MSF"; ?>
+<div class="col-lg-4" id="anuncios">
+    <div class="row">
+        <p>Profesionales a tu servicio</p>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <a href="http://msfarreos.com" target="_blank"> <img src="<?php echo($path);?>/img/clasificados/1.jpg" class="img-responsive"/> </a>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="<?php echo($path);?>/img/clasificados/2.jpg" class="img-responsive"/>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="<?php echo($path);?>/img/clasificados/4.jpg" class="img-responsive"/>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+        </div> 
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+        </div> 
+    </div>
+</div>
