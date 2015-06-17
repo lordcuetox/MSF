d5d9eb5b9692abbbfd6a8d3e2c823befb313eed2
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-17 11:54:24 -0500

    ../img/ajax-loading.gif

--- js/msf.js
+++ js/msf.js
@@ -1,5 +1,5 @@
 function getServiciosProfesionalesBy(letra) {
-    $("#ajax_results").html("<img src=\"img/ajax-loading.gif\" alt=\"Cargando\"/> Cargando ...");
+    $("#ajax_results").html("<img src=\"../img/ajax-loading.gif\" alt=\"Cargando\"/> Cargando ...");
     $("#ajax_results").load("../php/servicios_profesionales_ajax.php", {xAccion: "getServiciosProfesionales", l: letra});
 
 }
