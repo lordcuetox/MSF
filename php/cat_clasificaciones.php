a0527867576427250fc3f60cffda2a05548e2941
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-26 12:02:13 -0500

    Usan includeMenuAdmin.php

--- php/cat_clasificaciones.php
+++ php/cat_clasificaciones.php
@@ -67,44 +67,7 @@ if (isset($_POST['xAccion'])) {
         <![endif]-->
     </head>
     <div id="wrapper">
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Administrador de MSF</a>
-            </div>
-            <!-- /.navbar-header -->
-
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                            <li>
-                                <a href="cat_noticias.php"><i class="fa fa-crop"></i> Noticias</a>
-                                <a href="cat_eventos.php"><i class="fa fa-crop"></i> Eventos</a>
-                                <a href="cat_servicios_profesionales.php"><i class="fa fa-crop"></i> Servicios Profesionales</a>
-                                <a href="cat_profesiones.php"><i class="fa fa-crop"></i> Profesiones</a>
-                                <a href="cat_biblioteca.php"><i class="fa fa-crop"></i> Biblioteca</a>
-                                <a href="cat_ritos.php" ><i class="fa fa-university"></i> Ritos</a>
-                                <a href="cat_clasificaciones.php" ><i class="fa fa-leaf"></i> Clasificaciones</a>
-                                <a href="cat_grados.php"><i class="fa fa-crop"></i> Grados</a>
-                                <a href="cat_grandes_orientes.php"><i class="fa fa-crop"></i> Grandes Orientes</a>
-                                <a href="cat_grandes_logias.php"><i class="fa fa-crop"></i> Grandes Logias</a>
-                                <a href="cat_logias.php"><i class="fa fa-crop"></i> Logias</a>
-                                <a href="cat_reaton.php" class="active"><i class="fa fa-key"></i> Usuario y Contraseña</a>
-                                <a href="javascript:void(0);" onclick="logout();"><i class="fa fa-sign-out"></i> CERRAR SESIÓN</a>
-                            </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+        <?php $_GET['q'] = "clasificaciones"; include './includeMenuAdmin.php'; ?>
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
