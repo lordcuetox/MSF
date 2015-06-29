4953a757e094ad8b9be792dfbf2ffedefd16d138
WEBXICO-MX <weiss.uttab@gmail.com>
2015-06-29 10:42:07 -0500

    Se agrega miniatura para visualizar el logo y la función de subir imagenes.

--- php/cat_profesiones.php
+++ php/cat_profesiones.php
@@ -3,8 +3,7 @@ require_once '../clases/Profesion.php';
 require_once '../clases/UtilDB.php';
 session_start();
 
-if (!isset($_SESSION['cve_usuario'])) 
-{
+if (!isset($_SESSION['cve_usuario'])) {
     header('Location:login.php');
     return;
 }
@@ -27,11 +26,10 @@ if (isset($_POST['xAccion'])) {
         $profesiones->setActivo(isset($_POST['cbxActivo']) ? "1" : "0");
         $count = $profesiones->grabar();
     }
-       if ($_POST['xAccion'] == 'eliminar') {
+    if ($_POST['xAccion'] == 'eliminar') {
         $profesiones->borrar($_POST['txtCveProfesionEli']);
     }
-    if ($_POST['xAccion'] == 'logout')
-    {   
+    if ($_POST['xAccion'] == 'logout') {
         unset($_SESSION['cve_usuario']);
         header('Location:login.php');
         return;
@@ -66,7 +64,7 @@ $rst = UtilDB::ejecutaConsulta($sql);
     </head>
     <body>
         <div id="wrapper">
-            <?php $_GET['q'] = "profesiones"; include './includeMenuAdmin.php'; ?>
+            <?php $_GET['q'] = "profesiones";include './includeMenuAdmin.php'; ?>
             <div id="page-wrapper">
                 <div class="row">
                     <div class="col-lg-12">
@@ -80,10 +78,10 @@ $rst = UtilDB::ejecutaConsulta($sql);
                         <form role="form" name="frmProfesiones" id="frmProfesiones" action="cat_profesiones.php" method="POST">
                             <div class="form-group">
                                 <label for="txtCveProfesion"><input type="hidden" class="form-control" name="xAccion" id="xAccion" value="0" /></label>
-                                       <input type="hidden" class="form-control" id="txtCveProfesionEli" name="txtCveProfesionEli"  value="">    
+                                <input type="hidden" class="form-control" id="txtCveProfesionEli" name="txtCveProfesionEli"  value="">    
                                 <input type="hidden" class="form-control" id="txtCveProfesion" name="txtCveProfesion"
                                        placeholder="ID Profesión" value="<?php echo($profesiones->getCve_profesion()); ?>">
-                               
+
                             </div>
                             <div class="form-group">
                                 <label for="txtDescripcion">Descripción</label>
@@ -97,35 +95,44 @@ $rst = UtilDB::ejecutaConsulta($sql);
                             </div>
                             <button type="button" class="btn btn-default" id="btnLimpiar" name="btnLimpiar" onclick="limpiar();">Limpiar</button>
                             <button type="button" class="btn btn-default" id="btnGrabar" name="btnGrabar" onclick="grabar();">Enviar</button>
-                       
-                        <br/>
-                        <br/>
-                        <table class="table table-bordered table-striped table-hover table-responsive">
-                            <thead>
-                                <tr>
-                                    <th>ID Profesión</th>
-                                    <th>Descripción</th>
-                                    <th>Foto</th>
-                                    <th>Activo</th>
-                                    <th>Desactivar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($rst as $row) { ?>
+
+                            <br/>
+                            <br/>
+                            <table class="table table-bordered table-striped table-hover table-responsive">
+                                <thead>
                                     <tr>
-                                        <th><a href="javascript:void(0);" onclick="$('#txtCveProfesion').val(<?php echo($row['cve_profesion']); ?>);
-                                                    recargar();"><?php echo($row['cve_profesion']); ?></a></th>
-                                        <th><?php echo($row['descripcion']); ?></th>
-                                        <th><?php echo($row['logo']); ?></th>
-                                        <th><?php echo($row['activo'] == 1 ? "Si" : "No"); ?></th>
-                                        <th><button type="button" class="btn btn-default" id="btnEliminar" name="btnEliminar" onclick="eliminar(<?PHP echo $row['cve_profesion'];?>);">Desactivar</button></th>
+                                        <th>ID Profesión</th>
+                                        <th>Descripción</th>
+                                        <th>Foto</th>
+                                        <th>Activo</th>
+                                        <th>Desactivar</th>
                                     </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                         </form>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($rst as $row) { ?>
+                                        <tr>
+                                            <th><a href="javascript:void(0);" onclick="$('#txtCveProfesion').val(<?php echo($row['cve_profesion']); ?>);
+                                                    recargar();"><?php echo($row['cve_profesion']); ?></a></th>
+                                            <th><?php echo($row['descripcion']); ?></th>
+                                            <th><?php echo($row['logo'] != NULL ? "<img src=\"../img/File-JPG-icon.png\" alt=\"" . utf8_encode($row['descripcion']) . "\" title=\"" . $row['descripcion'] . "\" data-toggle=\"popover\" data-content=\"<img src='../" . $row['logo'] . "' alt='" . $row['descripcion'] . "' class='img-responsive'/>\" style=\"cursor:pointer;\"/><br/><a data-toggle=\"modal\" data-target=\"#myModal\" data-remote=\"cat_profesiones_upload_img.php?xCveProfesion=" . $row['cve_profesion'] . "\" href=\"javascript:void(0);\">Cambiar imagen</a>" : "<a data-toggle=\"modal\" data-target=\"#myModal\" data-remote=\"cat_profesiones_upload_img.php?xCveProfesion=" . $row['cve_profesion'] . "\" href=\"javascript:void(0);\">Subir imagen</a>"); ?></th>
+                                            <th><?php echo($row['activo'] == 1 ? "Si" : "No"); ?></th>
+                                            <th><button type="button" class="btn btn-default" id="btnEliminar" name="btnEliminar" onclick="eliminar(<?PHP echo $row['cve_profesion']; ?>);">Desactivar</button></th>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </form>
                     </div>
                     <div class="col-sm-4">&nbsp;</div>
+                </div>
+                <div class="row" >
+                    <div class="col-sm-12">
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>    
@@ -138,64 +145,84 @@ $rst = UtilDB::ejecutaConsulta($sql);
         <!-- Custom Theme JavaScript -->
         <script src="../twbs/plugins/startbootstrap-sb-admin-2-1.0.5/dist/js/sb-admin-2.js"></script>
         <script>
-        function logout()
-        {
-            $("#xAccion").val("logout");
-            $("#frmProfesiones").submit();
-        }
-            
-        function msg(opcion)
-        {
-            switch (opcion)
+            $(document).ready(function () {
+
+                $('[data-toggle="popover"]').popover({placement: 'top', html: true, trigger: 'click hover'});
+
+                /* Limpiar la ventana modal para volver a usar*/
+                $('body').on('hidden.bs.modal', '.modal', function () {
+                    $(this).removeData('bs.modal');
+                });
+
+            });
+
+            function logout()
             {
-                case 0:
-                    alert("[ERROR] Profesión no grabado");
-                    break;
-                case 1:
-                    alert("Profesión grabado con éxito!");
-                    break;
-                default:
-                    break;
+                $("#xAccion").val("logout");
+                $("#frmProfesiones").submit();
+            }
+
+            function msg(opcion)
+            {
+                switch (opcion)
+                {
+                    case 0:
+                        alert("[ERROR] Profesión no grabado");
+                        break;
+                    case 1:
+                        alert("Profesión grabado con éxito!");
+                        break;
+                    default:
+                        break;
+
+                }
 
             }
 
-        }
+            function limpiar()
+            {
+                $("#xAccion").val("0");
+                $("#txtCveProfesion").val("0");
+                $("#frmProfesiones").submit();
+            }
 
-        function limpiar()
-        {
-            $("#xAccion").val("0");
-            $("#txtCveProfesion").val("0");
-            $("#frmProfesiones").submit();
-        }
+            function grabar()
+            {
+                $("#xAccion").val("grabar");
+                $("#frmProfesiones").submit();
 
-        function grabar()
-        {
-            $("#xAccion").val("grabar");
-            $("#frmProfesiones").submit();
+            }
 
-        }
-        
-           function eliminar(valor)
-        {
-           
-            $("#xAccion").val("eliminar");
-            $("#txtCveProfesionEli").val(valor);
-            $("#frmProfesiones").submit();
+            function eliminar(valor)
+            {
 
-        }
+                $("#xAccion").val("eliminar");
+                $("#txtCveProfesionEli").val(valor);
+                $("#frmProfesiones").submit();
 
+            }
 
+            function recargar()
+            {
+                $("#xAccion").val("recargar");
+                $("#frmProfesiones").submit();
 
+            }
+            
+            function subir()
+            {
+                if ($("#fileToUpload").val() !== "")
+                {
+                    $("#xAccion2").val("upload");
+                    $("#frmUpload").submit();
+                }
+                else
+                {
+                    alert("No ha seleccionado un archivo para subir.");
+                }
+            }
 
-        function recargar()
-        {
-            $("#xAccion").val("recargar");
-            $("#frmProfesiones").submit();
-
-        }
-
-
-        msg(<?php echo($count) ?>);
+            msg(<?php echo($count) ?>);
         </script>
     </body>
 </html>
