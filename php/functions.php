8adade299818b9aa9d9d0d3210b72f6eccc9c316
WEBXICO-MX <weiss.uttab@gmail.com>
2015-07-01 14:49:51 -0500

    Se agrega getServiciosTextoCompleto y getServiciosTextoCompleto2

--- php/functions.php
+++ php/functions.php
@@ -9,6 +9,7 @@ function getParrafoTextoCompleto($numParrafos) {
     }
     return $texto_completo;
 }
+
 //Recibe un array con los ids de las cajas de texto
 function getParrafoTextoCompleto2($ids) {
     $texto_completo = "";
@@ -20,5 +21,30 @@ function getParrafoTextoCompleto2($ids) {
         }
     }
 
+    return $texto_completo;
+}
+
+function getServiciosTextoCompleto($numParrafos) {
+    $texto_completo = "<ul>";
+    for ($x = 1; $x <= $numParrafos; $x++) {
+        if (isset($_POST['txtParrafo' . $x])) {
+            $texto_completo .= "<li>" . $_POST['txtParrafo' . $x] . "</li>";
+        }
+    }
+    $texto_completo .= "</ul>";
+    return $texto_completo;
+}
+
+//Recibe un array con los ids de las cajas de texto
+function getServiciosTextoCompleto2($ids) {
+    $texto_completo = "<ul>";
+
+    foreach ($ids as $x) {
+
+        if (isset($_POST['txtParrafo' . $x])) {
+            $texto_completo .= "<li>" . $_POST['txtParrafo' . $x] . "</li>";
+        }
+    }
+    $texto_completo .= "</ul>";
     return $texto_completo;
 }
