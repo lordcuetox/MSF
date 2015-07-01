8024151ab5628f819a4b7bb33d5b18a49fa233d6
WEBXICO-MX <weiss.uttab@gmail.com>
2015-07-01 13:59:20 -0500

    Se agrega:
    function getNoticiaTextoCompleto2($ids)

--- php/functions.php
+++ php/functions.php
@@ -9,3 +9,16 @@ function getNoticiaTextoCompleto($numParrafos) {
     }
     return $texto_completo;
 }
+//Recibe un array con los ids de las cajas de texto
+function getNoticiaTextoCompleto2($ids) {
+    $texto_completo = "";
+
+    foreach ($ids as $x) {
+
+        if (isset($_POST['txtParrafo' . $x])) {
+            $texto_completo .= "<p>" . $_POST['txtParrafo' . $x] . "</p>";
+        }
+    }
+
+    return $texto_completo;
+}
