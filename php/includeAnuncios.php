3087a0458729ca1cf19f2d34467bfc00e7993650
WEBXICO-MX <weiss.uttab@gmail.com>
2015-07-06 14:32:54 -0500

    Modificaciones en el numero de columnas para cuando sea:
    1 .Extra small devices 
    2. Small devices
    3. Medium devices
    4. Large devices

--- php/includeAnuncios.php
+++ php/includeAnuncios.php
@@ -1,36 +1,41 @@
 <?php $path = ".."; ?>
 <div class="col-lg-4" id="anuncios">
+    <!--<div class="row">
+        <div class="col-md-12">
+            <h1 class="visible-lg">Large devices </h1>
+            <h1 class="visible-md">Medium devices</h1>
+            <h1 class="visible-sm">Small devices</h1>
+            <h1 class="visible-xs">Extra small devices </h1>
+        </div>
+    </div>-->
     <div class="row">
-        <p>Profesionales a tu servicio</p>
+        <div class="col-md-12">
+            <h1>Profesionales a tu servicio</h1>
+        </div>
     </div>
     <div class="row">
-        <div class="col-lg-12">
-            <a href="http://msfarreos.com" target="_blank"> <img src="<?php echo($path);?>/img/clasificados/1.jpg" class="img-responsive"/> </a>
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <a href="http://msfarreos.com" target="_blank"> <img src="<?php echo($path); ?>/img/clasificados/1.jpg" class="img-responsive"/> </a>
         </div> 
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <img src="<?php echo($path); ?>/img/clasificados/3.jpg" class="img-responsive"/>
         </div> 
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <img src="<?php echo($path); ?>/img/clasificados/3.jpg" class="img-responsive"/>
         </div> 
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <img src="<?php echo($path); ?>/img/clasificados/3.jpg" class="img-responsive"/>
         </div> 
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <img src="<?php echo($path); ?>/img/clasificados/3.jpg" class="img-responsive"/>
         </div> 
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <img src="<?php echo($path);?>/img/clasificados/3.jpg" class="img-responsive"/>
+
+        <div class="col-sm-6 col-md-4 col-lg-12">
+            <img src="<?php echo($path); ?>/img/clasificados/3.jpg" class="img-responsive"/>
         </div> 
     </div>
 </div>
