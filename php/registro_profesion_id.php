15985ef08f4a3e2f84ff9f6e03548dcb6bdc2d26
WEBXICO-MX <weiss.uttab@gmail.com>
2015-07-06 12:51:14 -0500

    Modificaciones para cuando el contacto este vacio y contenga http. Sin hay imagen se centra.

--- php/registro_profesion_id.php
+++ php/registro_profesion_id.php
@@ -9,6 +9,7 @@ $medio_contacto = NULL;
 $sql = "";
 $rst = NULL;
 $msg = "";
+$tmp = "";
 
 if (isset($_GET['id'])) {
     $profesion = new RegistroProfesion((int) $_GET['id']);
@@ -26,11 +27,11 @@ if ($profesion->getCve_profesion() > 0) {
     <div class="modal-body">
         <div class="te" style="list-style-position: inside;">
             <?php if ($profesion->getLogo() != "" && !(isset($_GET['view_only']))) { ?> 
-                <img src="../<?php echo($profesion->getLogo()); ?>" alt="<?php echo($profesion->getNombre_empresa()); ?>"/>
+                <img src="../<?php echo($profesion->getLogo()); ?>" alt="<?php echo($profesion->getNombre_empresa()); ?>" class="img-responsive" style="margin:0 auto;"/><br/>
             <?php } ?>
             <?php if (!(isset($_GET['view_only'])) || $_GET['view_only'] == 'servicios') { ?>
-            <p><strong>Servicios ofrecidos:</strong></p>
-            <?php echo($profesion->getServicios_ofrecidos()); ?>
+                <p><strong>Servicios ofrecidos:</strong></p>
+                <?php echo($profesion->getServicios_ofrecidos()); ?>
             <?php } ?>
             <?php if (!(isset($_GET['view_only']))) { ?>
                 <p><strong>Domicilio:</strong></p>
@@ -42,16 +43,27 @@ if ($profesion->getCve_profesion() > 0) {
                 $sql = "SELECT * FROM contactos_registros WHERE cve_registro =" . $profesion->getCve_registro();
                 $rst = UtilDB::ejecutaConsulta($sql);
                 if ($rst->rowCount() > 0) {
-                    echo("<p><strong>Contacto</strong></p>");
-                    echo("<ul>");
+                    $tmp .= "<p><strong>Contacto</strong></p>";
+                    $tmp .="<ul>";
                     foreach ($rst as $row) {
                         $contacto_registro = new ContactoRegistro($row['cve_contacto'], $row['cve_registro']);
-                        $medio_contacto = new MedioContacto($contacto_registro->getCve_contacto());
-                        echo("<li><img src=\"../" . $medio_contacto->getImagen() . "\" alt=\"" . $medio_contacto->getDescripcion() . "\"/> " . $contacto_registro->getDato() . "</li>");
+                        if ($contacto_registro->getDato() !== "") {
+                            $medio_contacto = new MedioContacto($contacto_registro->getCve_contacto());
+                            $tmp .="<li>";
+                            $tmp .="<img src=\"../" . $medio_contacto->getImagen() . "\" alt=\"" . $medio_contacto->getDescripcion() . "\" class=\"img-responsive\" style=\"display:inline-block; vertical-align:bottom;\"/>&nbsp;&nbsp;";
+
+                            if (strpos($contacto_registro->getDato(), 'http') !== false) {
+                                $tmp .= "<a href=\"".$contacto_registro->getDato()."\" target=\"_blank\">".$contacto_registro->getDato()."</a>";
+                            } else {
+                                $tmp .=$contacto_registro->getDato();
+                            }
+                            $tmp .="</li>";
+                        }
                     }
-                    echo("</ul>");
+                    $tmp .="</ul>";
                 }
                 $rst->closeCursor();
+                echo($tmp);
             }
             ?>
         </div>
