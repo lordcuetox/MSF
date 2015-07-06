3cf82f20493bff96f1c33a797ec1c702e27b141e
WEBXICO-MX <weiss.uttab@gmail.com>
2015-07-06 15:35:48 -0500

    Se agrega enlace a http://webxicoandcuetox.com/

--- php/includeFooter.php
+++ php/includeFooter.php
@@ -1,3 +1,3 @@
 <footer>
-    <p class="text-muted text-center">Copyright <?php echo date("Y"); ?>| Masonería Sin Fronteras| Powered By WEBXICO & Cuetox</p>
+    <p class="text-muted text-center">Copyright <?php echo date("Y"); ?>| Masonería Sin Fronteras| Powered By <a href="http://webxicoandcuetox.com/" target="_blank">WEBXICO & Cuetox</a></p>
 </footer>
