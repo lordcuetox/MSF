bfb28a3ec72b8487484756e2204fd6c30c641b53
lordcuetox <delcueto@gmail.com>
2015-08-07 14:16:57 -0500

    modificacion para usuarios de las logias

--- php/cat_logias.php
+++ php/cat_logias.php
@@ -74,6 +74,7 @@ if (isset($_POST['xAccion'])) {
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
+         <link href="../css/modal.css" rel="stylesheet"/>
     </head>
     <body>
         <?php include 'analyticstracking.php'; ?>
@@ -162,6 +163,7 @@ if (isset($_POST['xAccion'])) {
                         </div>
                     </div>
                 </div>
+ 
             </div>
         </div>
         <!-- jQuery -->
--- php/cat_logias_ajax.php
+++ php/cat_logias_ajax.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../clases/UtilDB.php';
+require_once '../clases/Prospectos.php';
 session_start();
 
 if (!isset($_SESSION['cve_usuario'])) {
@@ -7,6 +8,7 @@ if (!isset($_SESSION['cve_usuario'])) {
     return;
 }
 if (isset($_POST['cveOriente']) && isset($_POST['cveGranLogia'])) {
+
     $cveOriente = $_POST['cveOriente'];
     $cveGranLogia = $_POST['cveGranLogia'];
     $sql = "SELECT * from logias where cve_oriente=$cveOriente and cve_gran_logia=$cveGranLogia order by nombre";
@@ -37,11 +39,27 @@ if (isset($_POST['cveOriente']) && isset($_POST['cveGranLogia'])) {
                         <td><?php echo($row['direccion']); ?></td>
                         <td><?php echo($row['trabajos']); ?></td>
                         <td><?php echo($row['foto'] != NULL ? "<img src=\"../img/File-JPG-icon.png\" alt=\"" . utf8_encode($row['nombre']) . "\" title=\"" . $row['nombre'] . "\" data-toggle=\"popover\" data-content=\"<img src='../" . $row['foto'] . "' alt='" . $row['nombre'] . "' class='img-responsive'/>\" style=\"cursor:pointer;\"/><br/><br/><a data-toggle=\"modal\" data-target=\"#myModal\" data-remote=\"cat_logias_upload_img.php?xCveLogia=" . $row['cve_logia'] . "\" href=\"javascript:void(0);\">Cambiar imagen</a>" : "<a data-toggle=\"modal\" data-target=\"#myModal\" data-remote=\"cat_logias_upload_img.php?xCveLogia=" . $row['cve_logia'] . "\" href=\"javascript:void(0);\">Subir imagen</a>"); ?></td>
-                        <td><a href="#modal<?PHP echo(1); ?>">Nuevo</a></td>
+                        <td><a href="#modal<?PHP echo($row['cve_logia']); ?>">Agregar</a></td>
                         <td><a href="#modal<?PHP echo(2); ?>">Ver</a></td>
                         <td><?php echo($row['activo'] == 1 ? "Si" : "No"); ?></td>
                         <td><a href="javascript:void();" onclick="eliminar(<?PHP echo $row['cve_logia']; ?>);"> <i class="fa fa-trash-o"></i> </a></td>
                     </tr>
+                                               <!-- ventana modal de agrega usuarios-->
+            <div id="modal<?php echo($row['cve_logia']); ?>" class="modalmask">
+                <div class="modalbox resize">
+                    <a href="#close" title="Close" class="close">X</a>
+                    <h3 id="TituloModal">Agregar un nuevo usuario para la logia <?php echo($row['nombre']); ?></h3>
+                        <div class="form-group">
+                               <label for="txtNombre">Nombre:</label>
+                        <input type="text" class="form-control" id="txtNombre<?PHP echo $row['cve_logia'];?>" name="txtNombre<?PHP echo $row['cve_logia'];?>" 
+                               placeholder="Nombre " value="">
+                    </div>
+
+                </div>
+                
+            </div>
+                
+            <!--- fin de ventana modal de agrega usuarios-->
         <?php } $rst->closeCursor(); ?>
             </tbody>
         </table>
