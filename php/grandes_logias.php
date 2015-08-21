1ea69623f96853729bbe7c8ffdc8872953cd6f9e
WEBXICO-MX <weiss.uttab@gmail.com>
2015-08-21 15:40:20 -0500

    <div class="row"><div class="col-md-12"><h1> Grandes logias del Gran Oriente de <?php echo($gran_oriente->getDescripcion()); ?></h1></div></div>

--- php/grandes_logias.php
+++ php/grandes_logias.php
@@ -42,7 +42,7 @@ if (isset($_GET['go'])) {
             <?php include 'includeHeader.php'; ?>
             <div class="row">
                 <div class="col-lg-8">
-                    <div class="row"><div class="col-md-12"><h1> Gran Oriente de <?php echo($gran_oriente->getDescripcion()); ?></h1></div></div>
+                    <div class="row"><div class="col-md-12"><h1> Grandes logias del Gran Oriente de <?php echo($gran_oriente->getDescripcion()); ?></h1></div></div>
                     <div class="row">
                         <?php
                         $sql = "SELECT * FROM grandes_logias WHERE cve_oriente = " . $gran_oriente->getCveOriente();
