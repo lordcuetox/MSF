2ee83afd9f501e1e4a821485c23fc0d080a5eafc
WEBXICO-MX <weiss.uttab@gmail.com>
2015-08-24 12:28:18 -0500

    Se modifican los argumentos de la peticion ajax que realiza el metodo getVolumenesBy

--- js/msf.js
+++ js/msf.js
@@ -1,9 +1,9 @@
 function getServiciosProfesionalesBy(letra) {
     $("#ajax_results").html("<img src=\"../img/ajax-loading.gif\" alt=\"Cargando\"/> Cargando ...");
-    $("#ajax_results").load("../php/servicios_profesionales_ajax.php", {xAccion: "getServiciosProfesionales", l: letra});
+    $("#ajax_results").load("../php/servicios_profesionales_ajax.php", {xAccion: "getServiciosProfesionales", l:letra});
 }
 
-function getVolumenesBy(tipo,letra) {
+function getVolumenesBy(tipo,logia,grado,letra) {
     $("#ajax_results").html("<img src=\"../img/ajax-loading.gif\" alt=\"Cargando\"/> Cargando ...");
-    $("#ajax_results").load("../php/biblioteca2_ajax.php", {xAccion: "getVolumenes", t:tipo,l: letra});
+    $("#ajax_results").load("../php/biblioteca2_ajax.php", {xAccion: "getVolumenes", t:tipo,log:logia,gr:grado,l:letra});
 }
